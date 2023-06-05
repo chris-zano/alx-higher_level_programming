77b95de10e24d79b8f8e04c9de51e0326a7f663d
Christian Solomon <cncs.chris@gmail.com>
2023-06-05 15:57:20 +0000

    submission of 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -3,4 +3,3 @@
 str = "Python is an interpreted, interactive, object-oriented programming \
 language that combines remarkable power with very clear syntax"
 print(str[str.find("object"):str.find("language")] + "with Python\n", end="")
-
