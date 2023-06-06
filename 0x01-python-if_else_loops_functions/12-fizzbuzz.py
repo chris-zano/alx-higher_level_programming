48a708da4ac951577de76185b397ae69800871d6
Christian Solomon <cncs.chris@gmail.com>
2023-06-06 14:59:51 +0000

    submission of task

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 100):
+        if i % 3 == 0:
+            n = 'Fizz'
+            print("{:s}".format(n), end=' ')
+        elif i % 5 == 0:
+            n = 'Buzz'
+            print("{:s}".format(n), end=' ')
+        else:
+            print("{:d}".format(i), end=' ')
+    if (i + 1) % 3 == 0:
+        n = 'Fizz'
+        print("{:s}".format(n), end=' ')
+    elif (i + 1) % 5 == 0:
+        n = 'Buzz'
+        print("{:s}".format(n), end=' ')
+    else:
+        print("{:d}".format(i), end=' ')
+fizzbuzz()
