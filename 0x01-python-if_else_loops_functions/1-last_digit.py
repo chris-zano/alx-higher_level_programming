1cf9ab9431419cbfebf316b0a39543f0fe7063f3
Christian Solomon <cncs.chris@gmail.com>
2023-06-06 23:11:24 +0000

    correction of advanced and 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-n = int(str(abs(number))[-1])
-if n > 5:
-    check = 'and is greater than 5'
-elif n == 0:
-    check = 'and is 0'
+if number < 0:
+    remainder = number % -10
 else:
-    check = 'and is less than 6 and not 0'
-print(f"Last digit of {number} is {n} {check}")
+    remainder = number % 10
+print('Last digit of', number, 'is', remainder, end=' ')
+if remainder > 5:
+    print('and is greater than 5')
+elif remainder == 0:
+    print('and is 0')
+else:
+    print('and is less than 6 and not 0')
