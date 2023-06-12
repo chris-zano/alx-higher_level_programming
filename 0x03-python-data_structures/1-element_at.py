d8f6bad521ae836d835856bf4b42c6da13a0b3e6
Christian Solomon <cncs.chris@gmail.com>
2023-06-12 09:31:10 +0000

    submission of 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > len(my_list):
-        return None
-    elif idx < 0:
-        return None
-    else:
-        return my_list[idx]
+    return (my_list[idx] if 0 <= idx < len(my_list) else None)
