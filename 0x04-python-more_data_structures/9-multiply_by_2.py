e435b31c59c90dfeae0283435f938f8be70373b6
Christian Solomon <cncs.chris@gmail.com>
2023-06-14 19:30:33 +0000

    submission of 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    a = {}
     for key,value in a_dictionary.items():
-        a_dictionary[key] = a_dictionary[key] * 2
-    return a_dictionary
+        a[key] = a_dictionary[key] * 2
+    return a
