4b1d5b445ddbc10b53a0e9b47f027a6d5b4e1c4f
Christian Solomon <cncs.chris@gmail.com>
2023-06-14 22:26:35 +0000

    correction of 103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -3,23 +3,39 @@
 /**
  * print_python_list - Prints information about a Python list object.
  * @p: Pointer to the Python list object.
- * Return: void
  */
-
 void print_python_list(PyObject *p)
 {
 	Py_ssize_t size, i;
 	PyObject *item;
-	PyListObject *list = (PyListObject *)p;
+	PyObject *list_type = PyObject_Type(p);
+	PyObject *allocated_att;
+	Py_ssize_t allocated;
 
 	printf("[*] Python list info\n");
-	size = Py_SIZE(p);
+
+	size = PyObject_Length(p);
 	printf("[*] Size of the Python List = %zd\n", size);
-	printf("[*] Allocated = %zd\n", list->allocated);
+
+	if (list_type && PyType_Check(list_type))
+	{
+		allocated_att = PyObject_GetAttrString(list_type, "allocated");
+		allocated = PyLong_AsSsize_t(allocated_att);
+		Py_DECREF(list_type);
+		Py_XDECREF(allocated_att);
+
+		if (allocated >= 0)
+			printf("[*] Allocated = %zd\n", allocated);
+		else
+			printf("[*] Failed to retrieve the
+					'allocated' attribute\n");
+	}
+	else
+		printf("[*] Failed to retrieve the list type\n");
 
 	for (i = 0; i < size; i++)
 	{
-		item = PyList_GET_ITEM(p, i);
+		item = PyList_GetItem(p, i);
 		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
 	}
 }
@@ -27,14 +43,11 @@ void print_python_list(PyObject *p)
 /**
  * print_python_bytes - Prints information about a Python bytes object.
  * @p: Pointer to the Python bytes object.
- * Return: void
  */
-
 void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t size, i;
 	char *string;
-	PyBytesObject *bytes = (PyBytesObject *)p;
 
 	printf("[.] bytes object info\n");
 
@@ -44,7 +57,7 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	size = PyBytes_Size(p);
+	size = PyObject_Length(p);
 	string = PyBytes_AsString(p);
 
 	printf("  size: %zd\n", size);
@@ -55,3 +68,4 @@ void print_python_bytes(PyObject *p)
 		printf(" %02x", (unsigned char)string[i]);
 	printf("\n");
 }
+
