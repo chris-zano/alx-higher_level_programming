d00c6d77737e8584d65f5cb91e4795622406d47c
Christian Solomon <cncs.chris@gmail.com>
2023-06-14 20:56:41 +0000

    submission of 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     b = my_list[:]
-    return float(sum(n for n in [x * y for x, y in b]) / sum(y for x, y in b))
+    a = float(sum(n for n in [x * y for x, y in b]) / sum(y for x, y in b))
+    return a if my_list else 0.0
+
