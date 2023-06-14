d864b0ea0bfb3ae3d08010ad922e38d76199e658
Christian Solomon <cncs.chris@gmail.com>
2023-06-14 17:39:46 +0000

    correction of 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 def square_matrix_simple(matrix=[]):
-    return [list(map((lambda n: n ** 2), sublist)) for sublist in matrix]
+    return [list(map((lambda n: n * n), sublist)) for sublist in matrix]
