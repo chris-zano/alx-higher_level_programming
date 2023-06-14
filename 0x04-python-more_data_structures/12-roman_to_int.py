e1fe95502d6808e4cf03f710b04d3fe1b35aea33
Christian Solomon <cncs.chris@gmail.com>
2023-06-14 20:24:43 +0000

    submission of 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    a = roman_string[:]
+    roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                     'C': 100, 'D': 500, 'M': 1000}
+    number = 0
+    for i in range(len(a)):
+        if i + 1 < len(a) and roman_numbers[a[i]] < roman_numbers[a[i + 1]]:
+            number -= roman_numbers[a[i]]
+        else:
+            number += int(roman_numbers[a[i]])
+    return number
