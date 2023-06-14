ce2c772ebba06e47a706522bd5b50a8af8d23d32
Christian Solomon <cncs.chris@gmail.com>
2023-06-14 17:27:32 +0000

    submission of 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 def square_matrix_simple(matrix=[]):
     squared_list = []
-    for l in matrix:
+    for elmt in matrix:
         sublist = []
-        for n in l:
+        for n in elmt:
             sublist.append(n ** 2)
         squared_list.append(sublist)
     return squared_list
