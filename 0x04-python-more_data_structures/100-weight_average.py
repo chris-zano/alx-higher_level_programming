36806a53a1729f575fdc78cd5af5e2739ade965c
Christian Solomon <cncs.chris@gmail.com>
2023-06-14 20:52:26 +0000

    submission of 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,8 +2,3 @@
 def weight_average(my_list=[]):
     b = my_list[:]
     return float(sum(n for n in [x * y for x, y in b]) / sum(y for x, y in b))
-
-my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
-# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
-result = weight_average(my_list)
-print("Average: {:0.2f}".format(result))
