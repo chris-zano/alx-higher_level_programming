949e9541d1c1a69e93a4795fbc5d973063482066
Christian Solomon <cncs.chris@gmail.com>
2023-06-26 12:50:06 +0000

    correction@2_and_3.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,9 +3,8 @@ def safe_print_division(a, b):
     q = 0
     try:
         q = a / b
-    except (ZeroDivisionError, ValueError, TypeError):
-        return None
-    else:
-        return q
+    except ZeroDivisionError:
+        q = None
     finally:
         print("Inside result: {}".format(q))
+    return q
