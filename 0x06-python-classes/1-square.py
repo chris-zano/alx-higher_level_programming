9b9f7c685c10da5ff797ddc78d34dae2164f2214
Christian Solomon <cncs.chris@gmail.com>
2023-06-27 22:13:44 +0000

    submission of 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+1-square.py
+"""
+
+
+class Square:
+    """
+    Creates a square and defines a square based on size
+    """
+
+    def __init__(self, size):
+        """
+        creates an instance of the square
+        Args:
+            size: size of the square
+        """
+        self.__size = size
