9e53815789c5aaefbf410908c7d09d8112d7e1b9
Christian Solomon <cncs.chris@gmail.com>
2023-06-27 22:43:25 +0000

    submission of 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+4-square.py
+"""
+
+
+class Square:
+    """
+    Creates a square and defines a square based on optional size
+    """
+
+    def __init__(self, size=0):
+        """
+        creates an instance of the square
+        Args:
+            size: size of the square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        getter property to retrieve the size of the square
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        setter to set the value of the size of the square
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """
+        returns the computed value of the area
+        """
+
+        return self.__size ** 2
