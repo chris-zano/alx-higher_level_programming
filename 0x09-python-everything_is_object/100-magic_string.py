75e80b280ed34436a8c59c1c57fba554b2a1d849
Christian Solomon <cncs.chris@gmail.com>
2023-07-05 02:55:37 +0000

    submission of tasks

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("Holberton, " * (magic_string.n - 1) + "Holberton")
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return "BestSchool" + ", BestSchool" * (magic_string.count - 1)
+
