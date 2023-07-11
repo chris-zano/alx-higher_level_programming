f6ccf405ebf3fa5b185fe161cdc9065f3caf6bbb
Christian Solomon <cncs.chris@gmail.com>
2023-07-11 23:17:24 +0000

    submission@mandatory-3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is the 3-to_json_string.py
+This module provides only one function
+"""
+
+
+def to_json_string(my_obj):
+    """
+    This function returns the string representation of my_obj
+    Args:
+        my_obj (any): object
+    Returns:
+        json string of my_obj
+    """
+
+    if my_obj:
+        import json
+        return json.dumps(my_obj)
