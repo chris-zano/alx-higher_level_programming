708296101cade5f74673ba42553dfe30dcd5b08e
Christian Solomon <cncs.chris@gmail.com>
2023-07-11 01:02:45 +0000

    submission@2

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""6-base_geometry.py"""
+
+
+class BaseGeometry:
+    """Represents a geometric shape"""
+
+    def area(self):
+        """
+        Raises:
+            Exception with message 'area() is not implemented'
+        """
+        raise Exception("area() is not implemented")
