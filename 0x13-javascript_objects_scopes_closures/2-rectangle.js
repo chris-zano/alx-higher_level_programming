124bf91e8b7050655ecd309e82bdd6c18f388ab4
Christian Solomon <cncs.chris@gmail.com>
2024-03-12 09:55:24 +0000

    submission of tasks

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// check the value of w and h
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
