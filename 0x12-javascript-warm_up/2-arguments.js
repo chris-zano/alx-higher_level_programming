7afdce862bc59750d3fde9ffd58699cf4517c62e
Christian Solomon <cncs.chris@gmail.com>
2024-03-12 08:53:21 +0000

    submission of tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const { argv } = require('node:process');
 
-if (argv.length === 1) {
+if (argv.length === 0) {
   console.log('No argument');
-} else if (argv.length === 2) {
+} else if (argv.length < 2 ) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
